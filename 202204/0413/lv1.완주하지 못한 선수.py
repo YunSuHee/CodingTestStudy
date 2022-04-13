0735b1f6e327fca9bc20de7c9b6c33af35fbde09
YunSuHee <suheeyun97@gmail.com>
2022-04-13 23:35:10 +0900

    0413

--- 202204/0413/lv1.완주하지 못한 선수.py
+++ 202204/0413/lv1.완주하지 못한 선수.py
@@ -15,3 +15,11 @@
 # ["mislav", "stanko", "mislav", "ana"]	["stanko", "ana", "mislav"]	"mislav"
 
 
+def solution(participant,completion):
+    participant.sort()
+    completion.sort()
+
+    for p,c in zip(participant,completion):
+        if p != c:
+            return p
+    return participant.pop()
