11a3f53405f2a56fae449e5b12033951ebe6c224
YunSuHee <suheeyun97@gmail.com>
2022-04-19 00:41:19 +0900

    0418

--- 202204/0418/⭐lv1.소수만들기.py
+++ 202204/0418/⭐lv1.소수만들기.py
@@ -35,3 +35,18 @@ def isPrime(num):
         if num%i==0:
             return False
     return True
+
+#다른사람 풀이
+
+
+def solution(nums):
+    from itertools import combinations as cb
+    answer = 0
+    for a in cb(nums, 3):
+        cand = sum(a)
+        for j in range(2, cand):
+            if cand%j==0:
+                break
+        else:
+            answer += 1
+    return answer
---
+++ README.md
@@ -0,0 +1,23 @@
+#파이썬 자료구조, 함수
+#rjust, ljust 함수
+rjust( n , c=' ') : 문자열을 오른쪽으로 n만큼 정렬함. 빈칸은 c로 채워 넣는다.
+ljust( n , c=' ') : 문자열을 왼쪽으로 n만큼 정렬함. 빈칸은 c로 채워 넣는다.
+```
+a="abc"
+
+print(a.rjust(10))
+
+print(a.rjust(10,'#'))
+
+b="def"
+
+print(a.ljust(15))
+
+print(a.ljust(15,'k'))
+
+>>> 결과
+       abc
+#######abc
+abc            
+abckkkkkkkkkkkk
+```
