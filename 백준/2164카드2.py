42e55bd19ffb461451ecf62d666867dd0418a74f
YunSuHee <suheeyun97@gmail.com>
2022-04-26 23:58:11 +0900

    0426

--- 백준/2164카드2.py
+++ 백준/2164카드2.py
@@ -22,15 +22,14 @@
 from collections import deque
 n= int(input())
 
-q = deque()
-for i in range(1,n+1):
-    q.append(i)
-result = 0
-while len(q) != 1:
+q = deque([i for i in range(1, n+1)])
+
+while len(q) > 1:
     q.popleft()
     x=q.popleft()
     q.append(x)
-    if len(q) ==1:
-        result = x
 
-print(result)
+
+print(q[0])
+
+# 다시
