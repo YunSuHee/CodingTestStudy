a3da6062c4567c2ea2cd1bcc083fa9bfdbcd248e
YunSuHee <suheeyun97@gmail.com>
2022-05-03 21:42:48 +0900

    0503

--- 백준/1037 약수.py
+++ 백준/1037 약수.py
@@ -31,7 +31,8 @@
 n=int(input())
 arr = list(map(int,input().split()))
 arr.sort()
-if len(arr)==1:
-    print(arr[0]*arr[0])
-else:
-    print(arr[0]*arr[-1])
+print(arr[0]*arr[-1])
+# if len(arr)==1:
+#     print(arr[0]*arr[0])
+# else:
+#     print(arr[0]*arr[-1])
